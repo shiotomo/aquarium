3605b5aa60b96b093d08d08085c3161e33c26c57
shiotomo <t.shiota16@gmail.com>
2024-01-14 14:04:01 +0900

    .envで使用するportを決められるよう修正 & DBのバージョンを更新

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -2,8 +2,7 @@
 
 mkdir mysql5_data
 mkdir mysql8_data
-mkdir postgresql12_data
-mkdir postgresql9_data
-mkdir redis3_data
+mkdir postgresql13_data
+mkdir postgresql16_data
 mkdir redis5_data
 mkdir redis6_data
